10b1b86ab177861a0a47a2acf4afa68fad545529
VenkatasaiChalla <112607394+VenkatasaiChalla@users.noreply.github.com>
2022-09-28 23:26:40 +0530

    Delete min max problem.py

--- min max problem.py
+++
@@ -1,6 +0,0 @@
-# Python code to demonstrate the working of
-# max()
-
-# printing the maximum of 4,12,43.3,19,100
-print("Maximum of 4,12,43.3,19 and 100 is : ",end="")
-print (max( 4,12,43.3,19,100 ) )
